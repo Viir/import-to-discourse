6c2ab38d85ca35b1dec0556dc313cba5612a43ab
Michael Rätzel <Viir@Viir.de>
2016-11-05 23:10:41 +0100

    Locate lines to insert new records after.

--- discource-backup-add-topic/AddTopic.fs
+++ discource-backup-add-topic/AddTopic.fs
@@ -1,4 +1,16 @@
 ﻿module AddTopic
 
+open System.Text.RegularExpressions
+
+let indexOfElementMatchingRegexPattern regexPattern list =
+    list |> List.findIndex (fun elem -> Regex.IsMatch(elem, regexPattern, RegexOptions.IgnoreCase))
+
+let indexOfElementStartingWithRegexPattern regexPattern list =
+    indexOfElementMatchingRegexPattern ("^" + regexPattern) list
+
 let addTopic postgresqlDump =
+    let listLine = postgresqlDump |> Array.toList
+    let copyTopicsLineIndex = listLine |> indexOfElementStartingWithRegexPattern "COPY topics \("
+    let copyPostsLineIndex = listLine |> indexOfElementStartingWithRegexPattern "COPY posts \("
     postgresqlDump
+
