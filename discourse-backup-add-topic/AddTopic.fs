5ba0f1713ad57b86ba16f8352996b84dace4a527
Michael Rätzel <Viir@Viir.de>
2016-11-06 11:27:40 +0100

    This parses a copy section which adheres to the special cases for posts and topics observed in sql dumps from discourse version 1.7.?
    Example of a line from the dump from discourse:
    ----------------
    COPY topics (id, title, last_posted_at, created_at, updated_at, views, posts_count, user_id, last_post_user_id, reply_count, featured_user1_id, featured_user2_id, featured_user3_id, avg_time, deleted_at, highest_post_number, image_url, off_topic_count, like_count, incoming_link_count, bookmark_count, category_id, visible, moderator_posts_count, closed, archived, bumped_at, has_summary, vote_count, archetype, featured_user4_id, notify_moderators_count, spam_count, illegal_count, inappropriate_count, pinned_at, score, percent_rank, notify_user_count, subtype, slug, auto_close_at, auto_close_user_id, auto_close_started_at, deleted_by_id, participant_count, word_count, excerpt, pinned_globally, auto_close_based_on_last_post, auto_close_hours, pinned_until, fancy_title) FROM stdin;
    ----------------
    
    Terminology taken from https://www.postgresql.org/docs/9.6/static/sql-copy.html

--- discourse-backup-add-topic/AddTopic.fs
+++ discourse-backup-add-topic/AddTopic.fs
@@ -2,15 +2,39 @@
 
 open System.Text.RegularExpressions
 
+let recordValueSeparator = "\t"
+let columnNameSeparator = ","
+
+let topicsTableName = "topics"
+
+type CopySection = { tableName: string; startLineIndex : int; listLine : List<string>; listColumnName : List<string> }
+
 let indexOfElementMatchingRegexPattern regexPattern list =
     list |> List.findIndex (fun elem -> Regex.IsMatch(elem, regexPattern, RegexOptions.IgnoreCase))
 
 let indexOfElementStartingWithRegexPattern regexPattern list =
     indexOfElementMatchingRegexPattern ("^" + regexPattern) list
 
+let copySectionFromTableName tableName listLine =
+    let startLinePattern = "COPY\s+" + tableName + "\s*\(([^\)]*)\)"
+    let startLineIndex = listLine |> indexOfElementStartingWithRegexPattern startLinePattern
+    let copySectionListLine =
+        listLine
+        |> List.skip startLineIndex
+        |> List.takeWhile (fun line -> not (Regex.IsMatch(line, "^\\\.")))
+
+    let listColumnNameText = Regex.Match(copySectionListLine.Head, startLinePattern).Groups.[1].Value
+    let listColumnName =
+        listColumnNameText.Split(List.toArray [columnNameSeparator], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+        |> List.map (fun columnName -> columnName.Trim())
+
+    { tableName = tableName; startLineIndex = startLineIndex; listLine = copySectionListLine; listColumnName = List.empty }
+
 let addTopic postgresqlDump =
     let listLine = postgresqlDump |> Array.toList
-    let copyTopicsLineIndex = listLine |> indexOfElementStartingWithRegexPattern "COPY topics \("
-    let copyPostsLineIndex = listLine |> indexOfElementStartingWithRegexPattern "COPY posts \("
-    postgresqlDump
+
+    let topicsCopySection = listLine |> copySectionFromTableName topicsTableName
+
+    listLine
 
