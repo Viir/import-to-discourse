24cad0d53e0083788d62548f66f3a15e5bd6a6fe
Michael Rätzel <Viir@Viir.de>
2016-11-06 13:50:29 +0100

    Insert a topic record into the list of topic records. Not all the column values are covered yet. Therefore the emitted script is not valid for restore:
    ----------------
    After trying to restore from the modified backup, discourse reports:
    The restore has failed.
    
    Here's the log:
    [....]
    [2016-11-06 12:42:22] [STARTED]
    [2016-11-06 12:42:22] 'admin1' has started the restore!
    [2016-11-06 12:42:22] Marking restore as running...
    [2016-11-06 12:42:22] Making sure /var/www/discourse/tmp/restores/default/2016-11-06-124222 exists...
    [2016-11-06 12:42:22] Copying archive to tmp directory...
    [2016-11-06 12:42:23] Unzipping archive, this may take a while...
    [2016-11-06 12:42:23] Extracting metadata file...
    [2016-11-06 12:42:23] Validating metadata...
    [2016-11-06 12:42:23]   Current version: 20161102024920
    [2016-11-06 12:42:23]   Restored version: 20161102024920
    [2016-11-06 12:42:23] Extracting dump file...
    [2016-11-06 12:42:23] Restoring dump file... (can be quite long)
    [....]
    [2016-11-06 12:42:28] --------
    [2016-11-06 12:42:28] 20
    [2016-11-06 12:42:28] (1 row)
    [2016-11-06 12:42:28] COPY 5
    [2016-11-06 12:42:28] ERROR:  invalid input syntax for type timestamp: ""
    [2016-11-06 12:42:28] CONTEXT:  COPY topics, line 17, column updated_at: ""
    [2016-11-06 12:42:28] ERROR:  current transaction is aborted, commands ignored until end of transaction block
    [2016-11-06 12:42:28] ERROR:  current transaction is aborted, commands ignored until end of transaction block
    [2016-11-06 12:42:28] invalid command \.
    [2016-11-06 12:42:28] ERROR:  current transaction is aborted, commands ignored until end of transaction block
    [2016-11-06 12:42:28] ERROR:  current transaction is aborted, commands ignored until end of transaction block
    [2016-11-06 12:42:28] invalid command \.
    [....]
    [2016-11-06 12:42:29] invalid command \N
    [2016-11-06 12:42:29] EXCEPTION: psql failed
    [2016-11-06 12:42:29] invalid command \N
    [2016-11-06 12:42:29] /var/www/discourse/lib/backup_restore/restorer.rb:281:in `restore_dump'
    [....]

--- discourse-backup-add-topic/AddTopic.fs
+++ discourse-backup-add-topic/AddTopic.fs
@@ -1,13 +1,44 @@
 ﻿module AddTopic
 
+open System
+open System.Linq
 open System.Text.RegularExpressions
 
 let recordValueSeparator = "\t"
 let columnNameSeparator = ","
 
+let idColumnName = "id"
+
 let topicsTableName = "topics"
 
-type CopySection = { tableName: string; startLineIndex : int; listLine : List<string>; listColumnName : List<string> }
+type Topic =
+    {
+        userId: int;
+        title: string;
+        createdAt: DateTime;
+        lastPostedAt: DateTime;
+    }
+
+let topicToBeAdded =
+    {
+        userId = -1;
+        title = "topic which has been added by import tool";
+        createdAt = DateTime.UtcNow;
+        lastPostedAt = DateTime.UtcNow;
+    }
+
+type CopySection =
+    {
+        tableName: string;
+        startLineIndex : int;
+        listLine : List<string>;
+        listColumnName : List<string>;
+        listRecord : List<List<string * string>>
+    }
+
+let listColumnValueFromRecordLine (recordLine : string) =
+    recordLine.Split([recordValueSeparator] |> List.toArray, System.StringSplitOptions.None)
+    |> Array.toList
 
 let indexOfElementMatchingRegexPattern regexPattern list =
     list |> List.findIndex (fun elem -> Regex.IsMatch(elem, regexPattern, RegexOptions.IgnoreCase))
@@ -29,12 +60,83 @@ let copySectionFromTableName tableName listLine =
         |> Array.toList
         |> List.map (fun columnName -> columnName.Trim())
 
-    { tableName = tableName; startLineIndex = startLineIndex; listLine = copySectionListLine; listColumnName = List.empty }
+    let (_, listRecordLine) = copySectionListLine |> List.splitAt 1
+
+    let listRecordListColumnValue =
+        listRecordLine
+        |> List.map listColumnValueFromRecordLine
+
+    let listRecord =
+        listRecordListColumnValue
+        |> List.map (fun recordListColumnValue ->
+            recordListColumnValue
+            |> List.mapi (fun index columnValue ->
+                (listColumnName.ElementAt(index), columnValue)))
+
+    {
+        tableName = tableName;
+        startLineIndex = startLineIndex;
+        listLine = copySectionListLine;
+        listColumnName = listColumnName;
+        listRecord = listRecord
+    }
+
+let withRecordAdded copySectionOriginal funColumnValueFromName =
+    let recordListColumnNameAndValue =
+        copySectionOriginal.listColumnName
+        |> List.map (fun columnName -> (columnName, funColumnValueFromName columnName))
+    let recordLine =
+        String.Join(recordValueSeparator,
+            recordListColumnNameAndValue
+            |> List.map snd)
+    {
+        copySectionOriginal
+        with
+            listRecord = List.append copySectionOriginal.listRecord [ recordListColumnNameAndValue ];
+            listLine = List.append copySectionOriginal.listLine [ recordLine ]
+    }
+
+let withRecordAddedIdIncrement copySectionOriginal funColumnValueFromName =
+    let id =
+        copySectionOriginal.listRecord
+        |> List.map (fun record -> record |> List.find (fun (columnName, _) -> columnName = idColumnName))
+        |> List.map (fun (_, idString) -> System.Int32.Parse(idString))
+        |> List.max
+
+    withRecordAdded
+        copySectionOriginal
+        (fun columnName -> if columnName = idColumnName then (id + 1).ToString() else (funColumnValueFromName columnName))
+
+let timeString dateTime =
+    dateTime.ToString()
+
+let columnValueForTopic topic columnName =
+    match columnName with
+        | "user_id" -> topic.userId.ToString()
+        | "title" -> topic.title
+        | "created_at" -> timeString topic.createdAt
+        | "last_posted_at" -> timeString topic.lastPostedAt
+        | _ -> null
+
+let withRecordTopicAdded topic copySectionOriginal =
+    withRecordAddedIdIncrement
+        copySectionOriginal (columnValueForTopic topic)
+
+let withCopySectionReplaced sectionOriginalTableName sectionReplacement listLine =
+    let copySectionOriginal = listLine |> copySectionFromTableName sectionOriginalTableName
+    let (beforeListLine, afterBefore) = listLine |> List.splitAt copySectionOriginal.startLineIndex
+    let (_, afterListLine) = afterBefore |> List.splitAt copySectionOriginal.listLine.Length
+    [ beforeListLine; sectionReplacement.listLine; afterListLine ]
+    |> List.concat
 
 let addTopic postgresqlDump =
     let listLine = postgresqlDump |> Array.toList
 
     let topicsCopySection = listLine |> copySectionFromTableName topicsTableName
 
-    listLine
+    let topicsCopySectionUpdated = topicsCopySection |> withRecordTopicAdded topicToBeAdded
+
+    let listLineWithTopicAdded = listLine |> withCopySectionReplaced topicsTableName topicsCopySectionUpdated
+
+    listLineWithTopicAdded |> List.toArray
 
