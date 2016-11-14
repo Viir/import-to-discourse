3bbd0adb6d3f9890fc3e2c05505e9002540f68f8
Michael Rätzel <Viir@Viir.de>
2016-11-14 21:51:04 +0100

    Begin import from mvcforum. The sql script in the file "MvcForum.Export.To.Xml.sql" is used to export the data from a mvcforum database to an xml file.

---
+++ discourse-backup-add-topic/Import.mvcforum.fs
@@ -0,0 +1,70 @@
+﻿module Import.mvcforum
+
+open System
+open System.Linq
+open System.Xml
+
+let userXPath = "//SetMembershipUser/MembershipUser"
+
+type User = 
+    {
+        Id : string;
+        UserName : string;
+        Email : string;
+        CreateDate : System.DateTime;
+        LastLoginDate : System.DateTime Option;
+        LastActivityDate : System.DateTime Option;
+        Slug : string;
+    }
+
+let dictWithIndexAsKey listRecord =
+    listRecord |> List.mapi (fun record index -> (record, index)) |> dict
+
+let getValueFromSingleTextNode (xmlElement : XmlElement) =
+    if xmlElement <> null && xmlElement.ChildNodes.Count = 1
+    then
+        match xmlElement.ChildNodes.[0] with
+        | :? XmlText as text -> text.Value
+        | _ -> null
+    else null
+
+let getValueFromSingleChildElementWithName (xmlElement : XmlElement) childElementName =
+    let childElement = xmlElement.GetElementsByTagName(childElementName).OfType<XmlElement>().SingleOrDefault()
+    getValueFromSingleTextNode childElement
+
+let parseTimeOption timeString =
+    match timeString with
+    | null -> None
+    | _ -> Some (DateTime.Parse(timeString))
+
+let userFromXmlElement (xmlElement : XmlElement) =
+    let getColumnValue = getValueFromSingleChildElementWithName xmlElement
+    let getColumnTimeValue columnName = DateTime.Parse (getColumnValue columnName)
+    let getColumnTimeOptionValue columnName = parseTimeOption (getColumnValue columnName)
+
+    let id = getColumnValue "Id"
+    {
+        Id = id;
+        UserName = getColumnValue "UserName"
+        Email = getColumnValue "Email"
+        CreateDate = getColumnTimeValue "CreateDate"
+        LastLoginDate = getColumnTimeOptionValue "LastLoginDate"
+        LastActivityDate = getColumnTimeOptionValue "LastActivityDate"
+        Slug = getColumnValue "Slug"
+    }
+
+let import filePath =
+    let fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read)
+
+    let xmlDocument = new XmlDocument();
+
+    xmlDocument.Load(fileStream)
+
+    let setUserXmlNode = xmlDocument.SelectNodes(userXPath).OfType<XmlElement>() |> List.ofSeq
+
+    let listUser =
+        setUserXmlNode
+        |> List.map userFromXmlElement
+        |> List.sortBy (fun user -> user.CreateDate)
+
+    listUser
---
+++ discourse-backup-add-topic/MvcForum.Export.To.Xml.sql
@@ -0,0 +1,7 @@
+﻿SELECT
+    (SELECT * FROM dbo.MembershipUser FOR XML PATH('MembershipUser'), TYPE) AS 'SetMembershipUser',
+    (SELECT * FROM dbo.Category FOR XML PATH('Category'), TYPE) AS 'SetCategory',
+    (SELECT * FROM dbo.CategoryNotification FOR XML PATH('CategoryNotification'), TYPE) AS 'SetCategoryNotification',
+    (SELECT * FROM dbo.Topic FOR XML PATH('Topic'), TYPE) AS 'SetTopic',
+    (SELECT * FROM dbo.Post FOR XML PATH('Post'), TYPE) AS 'SetPost'
+FOR XML PATH(''), ROOT('root')
