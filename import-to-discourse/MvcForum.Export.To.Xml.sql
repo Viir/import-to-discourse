0f347ed6789aa8155ff76309712737d609254cd8
Michael Rätzel <Viir@Viir.de>
2016-11-26 10:37:16 +0100

    Prepare importing votes and tags: Add export of votes and tags.

--- import-to-discourse/MvcForum.Export.To.Xml.sql
+++ import-to-discourse/MvcForum.Export.To.Xml.sql
@@ -3,5 +3,8 @@
     (SELECT * FROM dbo.Category FOR XML PATH('Category'), TYPE) AS 'SetCategory',
     (SELECT * FROM dbo.CategoryNotification FOR XML PATH('CategoryNotification'), TYPE) AS 'SetCategoryNotification',
     (SELECT * FROM dbo.Topic FOR XML PATH('Topic'), TYPE) AS 'SetTopic',
-    (SELECT * FROM dbo.Post FOR XML PATH('Post'), TYPE) AS 'SetPost'
+    (SELECT * FROM dbo.Post FOR XML PATH('Post'), TYPE) AS 'SetPost',
+    (SELECT * FROM dbo.TopicTag FOR XML PATH('TopicTag'), TYPE) AS 'SetTopicTag',
+    (SELECT * FROM dbo.Topic_Tag FOR XML PATH('Topic_Tag'), TYPE) AS 'SetTopic_Tag',
+    (SELECT * FROM dbo.Vote FOR XML PATH('Vote'), TYPE) AS 'SetVote'
 FOR XML PATH(''), ROOT('root')
